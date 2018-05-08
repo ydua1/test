8dd97a91be86416285483f3efddea5acae902790
Yash Dua <ydua@ucsc.edu>
2018-05-08 16:30:07 -0700

    Site template done:

---
+++ main.js
@@ -0,0 +1,9 @@
+var game = new Phaser.Game(800, 600, Phaser.AUTO);
+var titleScreen = function(game) {}; // title screen state
+titleScreen.prototype = {}
+
+var gamePlay = function(game) {};
+gamePlay.prototype = {}
+
+var gameOver = function(game) {};
+gameOver.prototype = {}
